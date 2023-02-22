1501b938bc754106646ae3a617ad914af0d06356
CrispityBits <peytonkringle@gmail.com>
2023-02-22 12:42:38 -0600

    Finished function newFolderName

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const newFolderName = function (folders) {
+  newFolderCount = 0;
+  for(i = 0; i < folders.length; i++) {
+    if(folders[i].includes("New Folder")) {
+      newFolderCount += 1;
+    }
+  }
 
+  
+  if(newFolderCount != 0) {
+    newFolderCount += 1;
+  }
+
+  return(`New Folder (${newFolderCount})`);
 }
+
+console.log(newFolderName(["New Folder", "New Folder (2)", "New Folder (3)", "Folder Name", "Filler Name", "New Folder (4)"]));
